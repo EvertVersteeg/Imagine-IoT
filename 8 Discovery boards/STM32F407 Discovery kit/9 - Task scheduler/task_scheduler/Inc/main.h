e7eb8f000f742ec0d8e562cd6482c291ae3ca243
Evert Versteeg <eversteeg@imaginebv.nl>
2021-04-17 17:24:41 +0200

    Added task schedular

---
+++ 8 Discovery boards/STM32F407 Discovery kit/9 - Task scheduler/task_scheduler/Inc/main.h
@@ -0,0 +1,31 @@
+/*
+ * main.h
+ *
+ *  Created on: 17 apr. 2021
+ *      Author: evers
+ */
+
+#ifndef MAIN_H_
+#define MAIN_H_
+
+#define MAX_TASKS			4
+
+/* Some stack memory calulations */
+#define SIZE_TASK_STACK		1024U
+#define SIZE_SCHED_STACK	1024U
+
+#define SRAM_START			0x20000000U
+#define SIZE_SRAM			( (128) * (1024) )
+#define SRAM_END			( (SRAM_START) + (SIZE_SRAM) )
+
+#define T1_STACK_START		(SRAM_END)
+#define T2_STACK_START		( (SRAM_END) - (1 * SIZE_TASK_STACK))
+#define T3_STACK_START		( (SRAM_END) - (2 * SIZE_TASK_STACK))
+#define T4_STACK_START		( (SRAM_END) - (3 * SIZE_TASK_STACK))
+#define SCHED_STACK_START		( (SRAM_END) - (4 * SIZE_TASK_STACK))
+#define DUMMY_XPSR 0x01000000U
+#define TICK_HZ				1000U
+#define HSI_CLOCK			16000000U
+#define SYSTICK_TIM_CLK		HSI_CLOCK
+
+#endif /* MAIN_H_ */
