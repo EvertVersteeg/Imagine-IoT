98d99d603886377ec1e7cb6c4affd213f89c1b34
Evert Versteeg <eversteeg@imaginebv.nl>
2021-05-16 21:16:21 +0200

    Updated drivers header file

--- 8 Discovery boards/STM32F407 Discovery kit/11 - Default program/DefaultProgram/Debug/Src/main.d
+++ 8 Discovery boards/STM32F407 Discovery kit/11 - Default program/DefaultProgram/Debug/Src/main.d
@@ -1,3 +1,6 @@
-Src/main.o: ../Src/main.c ../Src/main.h
+Src/main.o: ../Src/main.c ../Src/main.h \
+ C:/Users/evers/Documents/IMAGINE-IOT/Imagine-IoT/Imagine-IoT/8\ Discovery\ boards/STM32F407\ Discovery\ kit/11\ -\ Default\ program/DefaultProgram/Drivers/Inc/stm32f407xx.h
 
 ../Src/main.h:
+
+C:/Users/evers/Documents/IMAGINE-IOT/Imagine-IoT/Imagine-IoT/8\ Discovery\ boards/STM32F407\ Discovery\ kit/11\ -\ Default\ program/DefaultProgram/Drivers/Inc/stm32f407xx.h:
--- 8 Discovery boards/STM32F407 Discovery kit/11 - Default program/DefaultProgram/Debug/sources.mk
+++ 8 Discovery boards/STM32F407 Discovery kit/11 - Default program/DefaultProgram/Debug/sources.mk
@@ -20,6 +20,7 @@ OBJCOPY_BIN :=
 
 # Every subdirectory with source files must be described here
 SUBDIRS := \
+Drivers/Inc \
 Src \
 Startup \
 
