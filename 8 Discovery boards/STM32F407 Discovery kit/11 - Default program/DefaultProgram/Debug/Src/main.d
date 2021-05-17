2a812d407a700ba00169bb1995b26645780c50da
Evert Versteeg <eversteeg@imaginebv.nl>
2021-05-17 17:24:07 +0200

    Updated comments + removed main.h

--- 8 Discovery boards/STM32F407 Discovery kit/11 - Default program/DefaultProgram/Debug/Src/main.d
+++ 8 Discovery boards/STM32F407 Discovery kit/11 - Default program/DefaultProgram/Debug/Src/main.d
@@ -1,6 +1,4 @@
-Src/main.o: ../Src/main.c ../Src/main.h \
+Src/main.o: ../Src/main.c \
  C:/Users/evers/Documents/IMAGINE-IOT/Imagine-IoT/Imagine-IoT/8\ Discovery\ boards/STM32F407\ Discovery\ kit/11\ -\ Default\ program/DefaultProgram/Drivers/Inc/stm32f407xx.h
-
-../Src/main.h:
 
 C:/Users/evers/Documents/IMAGINE-IOT/Imagine-IoT/Imagine-IoT/8\ Discovery\ boards/STM32F407\ Discovery\ kit/11\ -\ Default\ program/DefaultProgram/Drivers/Inc/stm32f407xx.h:
