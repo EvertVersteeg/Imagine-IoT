97e6b16bfecfc39f56678890e4e65206c352abcf
Evert Versteeg <eversteeg@imaginebv.nl>
2021-07-13 11:57:35 +0200

    Macros and function defenition (GPIO API)

--- 8 Discovery boards/STM32F407 Discovery kit/11 - Default program/DefaultProgram/Drivers/Inc/stm32f407xx_gpio_driver.h
+++ 8 Discovery boards/STM32F407 Discovery kit/11 - Default program/DefaultProgram/Drivers/Inc/stm32f407xx_gpio_driver.h
@@ -1,15 +1,17 @@
-/*
- * stm32f407xx_gpio_driver.h
- *
+
+/*************************************************************************************************************************************************************
+ *                           GPIO API header file
+ *************************************************************************************************************************************************************
  * GPIO initialization
  * Enable/Disable GPIO port clock
- * Read From a GPIO pin
- * Write to a GPIO pin
+ * Read From a GPIO pin or port
+ * Write to a GPIO pin or port
  * Configure alternate functionality
  * Interrupt handling
  *
  *  Created on: 24 mei 2021
- *      Author: evers
+ *      Author: E. Versteeg
+ *      Imagine Solutions BV
  */
 
 #ifndef INC_STM32F407XX_GPIO_DRIVER_H_
@@ -36,16 +38,58 @@ typedef struct
 
 }GPIO_Handle_t;
 
+/*
+ * GPIO pin possible modes
+ */
+#define GPIO_MODE_IN 		0
+#define GPIO_MODE_OUT 		1
+#define GPIO_MODE_ALTFN 	2
+#define GPIO_MODE_ANALOG 	3
+#define GPIO_MODE_IT_FT		4		//interrupt input falling edge
+#define GPIO_MODE_IT_RT		5		//interrupt input falling edge
+#define GPIO_MODE_IT_RFT	6		//interrupt input rising edge falling edge trigger
 
-// ***** APIs supported by this driver
-void GPIO_PeriClockControl(GPIO_RegDef_t *pGIOx, uint8_t EnorDi);							// Peripheral clock configuration
+/*
+ * GPIO possible output types
+ */
+#define GPIO_OP_TYPE_PP		0		//Output type push pull
+#define GPIO_OP_TYPE_OD		1		//Output type open drain
+
+/*
+ * GPIO possible output types
+ */
+#define GPIO_SPEED_LOW		0
+#define GPIO_SPEED_MEDIUM	1
+#define GPIO_SPEED_FAST		2
+#define GPIO_SPEED_HIGH		3
+
+
+/*************************************************************************************************************************************************************
+ *                           APIs supported by this driver
+ *         For more information about th APIs check the function definition
+ *************************************************************************************************************************************************************
+ * Peripheral Clock setup
+ */
+void GPIO_PeriClockControl(GPIO_RegDef_t *pGIOx, uint8_t EnorDi);							// Peripheral clock configuration  EnorDi => Enable or Disable
+
+/*
+ * Init and de-init GPIO
+ */
 void GPIO_Init(GPIO_Handle_t *pGIOx);														// GPIO Init
-void GPIO_DeInit(GPIO_RegDef_t *pGPIOx);													// De Init
+void GPIO_DeInit(GPIO_RegDef_t *pGPIOx);													// De Init, set register to its default state using peripheral reset register (RCC_AHB1RSTR)
+
+/*
+ * Data read and write
+ */
 uint8_t GPIO_ReadFromInputPin(GPIO_RegDef_t *pGPIOx, uint8_t PinNumber);					// Read from input pin
 uint16_t GPIO_ReadFromInputPort(GPIO_RegDef_t *pGPIOx);										// Read from input port
 void GPIO_WriteToOutputPin(GPIO_RegDef_t *pGPIOx, uint16_t PinNumber, uint8_t Value);		// Write to output pin
 void GPIO_WriteToOutputPort(GPIO_RegDef_t *pGPIOx, uint8_t Value);							// Write to output port
 void GPIO_ToggleOutputPin(GPIO_RegDef_t *pGPIOx, uint16_t PinNumber);						// Toggle output pin
+
+/*
+ * IRQ Configuration and ISR handling
+ */
 void GPIO_IRQConfig(uint8_t IRQNumber, uint8_t IRQPriority, uint8_t EnorDi);				// IRQ Configuration and ISR handling
 void GPIO_IRQHandling(uint8_t PinNumber);													// IRQ Handling
 
