3405ba87d8992364e3943aec0b873a57f671e497
Evert Versteeg <eversteeg@imaginebv.nl>
2021-07-16 21:48:56 +0200

    Toggle green LED with GPIO API

--- 8 Discovery boards/STM32F407 Discovery kit/11 - Default program/DefaultProgram/Debug/Drivers/Src/stm32f407xx_gpio_driver.d
+++ 8 Discovery boards/STM32F407 Discovery kit/11 - Default program/DefaultProgram/Debug/Drivers/Src/stm32f407xx_gpio_driver.d
@@ -1,8 +1,11 @@
 Drivers/Src/stm32f407xx_gpio_driver.o: \
  ../Drivers/Src/stm32f407xx_gpio_driver.c \
  C:/Users/evers/Documents/IMAGINE-IOT/Imagine-IoT/Imagine-IoT/8\ Discovery\ boards/STM32F407\ Discovery\ kit/11\ -\ Default\ program/DefaultProgram/Drivers/Inc/stm32f407xx_gpio_driver.h \
- C:/Users/evers/Documents/IMAGINE-IOT/Imagine-IoT/Imagine-IoT/8\ Discovery\ boards/STM32F407\ Discovery\ kit/11\ -\ Default\ program/DefaultProgram/Drivers/Inc/stm32f407xx.h
+ C:/Users/evers/Documents/IMAGINE-IOT/Imagine-IoT/Imagine-IoT/8\ Discovery\ boards/STM32F407\ Discovery\ kit/11\ -\ Default\ program/DefaultProgram/Drivers/Inc/stm32f407xx.h \
+ C:/Users/evers/Documents/IMAGINE-IOT/Imagine-IoT/Imagine-IoT/8\ Discovery\ boards/STM32F407\ Discovery\ kit/11\ -\ Default\ program/DefaultProgram/Drivers/Inc/stm32f407xx_gpio_driver.h
 
 C:/Users/evers/Documents/IMAGINE-IOT/Imagine-IoT/Imagine-IoT/8\ Discovery\ boards/STM32F407\ Discovery\ kit/11\ -\ Default\ program/DefaultProgram/Drivers/Inc/stm32f407xx_gpio_driver.h:
 
 C:/Users/evers/Documents/IMAGINE-IOT/Imagine-IoT/Imagine-IoT/8\ Discovery\ boards/STM32F407\ Discovery\ kit/11\ -\ Default\ program/DefaultProgram/Drivers/Inc/stm32f407xx.h:
+
+C:/Users/evers/Documents/IMAGINE-IOT/Imagine-IoT/Imagine-IoT/8\ Discovery\ boards/STM32F407\ Discovery\ kit/11\ -\ Default\ program/DefaultProgram/Drivers/Inc/stm32f407xx_gpio_driver.h:
--- 8 Discovery boards/STM32F407 Discovery kit/11 - Default program/DefaultProgram/Debug/Src/main.d
+++ 8 Discovery boards/STM32F407 Discovery kit/11 - Default program/DefaultProgram/Debug/Src/main.d
@@ -1,4 +1,10 @@
 Src/main.o: ../Src/main.c \
+ C:/Users/evers/Documents/IMAGINE-IOT/Imagine-IoT/Imagine-IoT/8\ Discovery\ boards/STM32F407\ Discovery\ kit/11\ -\ Default\ program/DefaultProgram/Drivers/Inc/stm32f407xx.h \
+ C:/Users/evers/Documents/IMAGINE-IOT/Imagine-IoT/Imagine-IoT/8\ Discovery\ boards/STM32F407\ Discovery\ kit/11\ -\ Default\ program/DefaultProgram/Drivers/Inc/stm32f407xx_gpio_driver.h \
  C:/Users/evers/Documents/IMAGINE-IOT/Imagine-IoT/Imagine-IoT/8\ Discovery\ boards/STM32F407\ Discovery\ kit/11\ -\ Default\ program/DefaultProgram/Drivers/Inc/stm32f407xx.h
+
+C:/Users/evers/Documents/IMAGINE-IOT/Imagine-IoT/Imagine-IoT/8\ Discovery\ boards/STM32F407\ Discovery\ kit/11\ -\ Default\ program/DefaultProgram/Drivers/Inc/stm32f407xx.h:
+
+C:/Users/evers/Documents/IMAGINE-IOT/Imagine-IoT/Imagine-IoT/8\ Discovery\ boards/STM32F407\ Discovery\ kit/11\ -\ Default\ program/DefaultProgram/Drivers/Inc/stm32f407xx_gpio_driver.h:
 
 C:/Users/evers/Documents/IMAGINE-IOT/Imagine-IoT/Imagine-IoT/8\ Discovery\ boards/STM32F407\ Discovery\ kit/11\ -\ Default\ program/DefaultProgram/Drivers/Inc/stm32f407xx.h:
