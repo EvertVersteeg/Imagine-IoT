4c484ea62832328a7c1b25e2390aa91b6cef0dd2
Evert Versteeg <eversteeg@imaginebv.nl>
2021-08-10 16:32:38 +0200

    Timestamp to CSV and in filename

--- 08 lora-nodes Arduino MKR 1300/ReadAndStoreSensors/ReadAndStoreSensors.ino
+++ 08 lora-nodes Arduino MKR 1300/ReadAndStoreSensors/ReadAndStoreSensors.ino
@@ -20,67 +20,87 @@ float humidity    = 0;
 float pressure    = 0;
 float illuminance = 0;
 const byte seconds = 0;
-const byte minutes = 0;
+const byte minutes = 10;
 const byte hours = 16;
 /* Change these values to set the current initial date */
 const byte day = 10;
 const byte month = 8;
 const byte year = 21;
+char filename[] = "000000.CSV";
+byte actualDay =0;
+byte currentDay =0;
 File dataFile;
 RTCZero rtc;
 
 
 void setup() {
+  // init serial
   Serial.begin(9600);
   while (!Serial);
+  // Init time and date
   rtc.begin(); // initialize RTC
-  // Set the time
   rtc.setHours(hours);
   rtc.setMinutes(minutes);
   rtc.setSeconds(seconds);
-  // Set the date
   rtc.setDay(day);
   rtc.setMonth(month);
   rtc.setYear(year);
+  // Check shield
   if (!ENV.begin()) {
     Serial.println("Failed to initialize MKR ENV shield!");
     while (1);
   }
-    // init SPI
+  // init SPI and SD card
   SPI.begin();
   delay(100);
-
-  // init SD card
   if(!SD.begin(SD_CS_PIN)) {
     Serial.println("Failed to initialize SD card!");
     while (1);
   }
-
   // init the logfile
-  dataFile = SD.open("log-0000.csv", FILE_WRITE);
+  getFileName();
+  dataFile = SD.open(filename, FILE_WRITE);
   delay(1000);
-
   // init the CSV file with headers
-  dataFile.println("temperature,humidity,pressure,illuminance");
-
+  dataFile.println("date,time,temperature,humidity,pressure,illuminance");
+  currentDay = rtc.getDay();
   // close the file
   dataFile.close();
   delay(100);
+  Serial.println("Opened first file");
 }
 
 void loop() {
-  Serial.println("Open file");
+  actualDay = rtc.getDay();
+  if (currentDay != actualDay)
+  {
+    // Add new file to SD at start of new dat
+    getFileName();
+    dataFile = SD.open(filename, FILE_WRITE);
+    delay(1000);
+    dataFile.println("date,time,temperature,humidity,pressure,illuminance");
+    currentDay = actualDay;
+    // close the file
+    dataFile.close();
+    delay(100);
+    Serial.println("Opened a new file");
+  }
   // init the logfile
-  dataFile = SD.open("log-0000.csv", FILE_WRITE);
+  getFileName();
+  dataFile = SD.open(filename, FILE_WRITE);
   delay(1000);
-  
   // read all the sensor values
   temperature = ENV.readTemperature();
   humidity    = ENV.readHumidity();
   pressure    = ENV.readPressure();
   illuminance = ENV.readIlluminance();
-  
-  // print each of the sensor values
+  String time = (String(rtc.getHours())+":"+String(rtc.getMinutes())+":"+String(rtc.getSeconds()));
+  String date = (String(rtc.getDay())+"/"+String(rtc.getMonth())+"/"+String(rtc.getYear()));
+  // Save each of the sensor values to file
+  dataFile.print(date);
+  dataFile.print(",");
+  dataFile.print(time);
+  dataFile.print(",");
   dataFile.print(temperature);
   dataFile.print(",");
   dataFile.print(humidity);
@@ -88,11 +108,9 @@ void loop() {
   dataFile.print(pressure);
   dataFile.print(",");
   dataFile.println(illuminance);
-
   // close the file
   dataFile.close();
-  Serial.println("Close file");
-
+  Serial.println("Added data to file");
   // print each of the sensor values
   Serial.print("Temperature = ");
   Serial.print(temperature);
@@ -106,36 +124,17 @@ void loop() {
   Serial.print("Illuminance = ");
   Serial.print(illuminance);
   Serial.println(" lx");
-  // print an empty line
   Serial.println();
-  // Print date...
-
-  Serial.print(rtc.getDay());
-
-  Serial.print("/");
-
-  Serial.print(rtc.getMonth());
-
-  Serial.print("/");
-
-  Serial.print(rtc.getYear());
-
-  Serial.print(" ");
-
-  // ...and time
-
-  Serial.print(rtc.getHours());
-
-  Serial.print(":");
-
-  Serial.print(rtc.getMinutes());
-
-  Serial.print(":");
-
-  Serial.print(rtc.getSeconds());
-
-  Serial.println();
-
   // wait 5 second to print again
   delay(5000);
 }
+
+void getFileName(){
+  //Build a new filename
+  filename[0] = rtc.getDay()/10 + '0'; //To get 1st digit from day()
+  filename[1] = rtc.getDay()%10 + '0'; //To get 2nd digit from day()
+  filename[2] = rtc.getMonth()/10 + '0'; //To get 1st digit from month()
+  filename[3] = rtc.getMonth()%10 + '0'; //To get 2nd digit from month()
+  filename[4] = rtc.getYear()/10 + '0'; //To get 3rd digit from year()
+  filename[5] = rtc.getYear()%10 + '0'; //To get 4th digit from year()
+  }
